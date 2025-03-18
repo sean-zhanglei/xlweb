077249cf3852cbb29f507a130ff8b95b24fa6d2b
SeanChang <mmmmzhanglei@gmail.com>
2025-03-18 22:57:11 +0800

    xlweb-mico 成功返回200 而不是 0

--- mico/xlweb-micoservice/xlweb-mico-common/src/main/java/com/nbug/mico/common/exception/enums/GlobalErrorCodeConstants.java
+++ mico/xlweb-micoservice/xlweb-mico-common/src/main/java/com/nbug/mico/common/exception/enums/GlobalErrorCodeConstants.java
@@ -9,13 +9,12 @@ import com.nbug.mico.common.exception.ErrorCode;
  *
  * 一般情况下，使用 HTTP 响应状态码 https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status
  * 虽然说，HTTP 响应状态码作为业务使用表达能力偏弱，但是使用在系统层面还是非常不错的
- * 比较特殊的是，因为之前一直使用 0 作为成功，就不使用 200 啦。
  *
  * @author NBUG
  */
 public interface GlobalErrorCodeConstants {
 
-    ErrorCode SUCCESS = new ErrorCode(0, "成功");
+    ErrorCode SUCCESS = new ErrorCode(200, "成功");
 
     // ========== 客户端错误段 ==========
 
