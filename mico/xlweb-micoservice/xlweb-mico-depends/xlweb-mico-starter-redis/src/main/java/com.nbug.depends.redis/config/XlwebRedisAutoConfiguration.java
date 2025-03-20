efc47b564a3ef1b30a330740331d1a95ad46a6dc
SeanChang <mmmmzhanglei@gmail.com>
2025-03-20 14:00:44 +0800

    xlweb-mico fix redis res

--- mico/xlweb-micoservice/xlweb-mico-depends/xlweb-mico-starter-redis/src/main/java/com.nbug.depends.redis/config/XlwebRedisAutoConfiguration.java
+++ mico/xlweb-micoservice/xlweb-mico-depends/xlweb-mico-starter-redis/src/main/java/com.nbug.depends.redis/config/XlwebRedisAutoConfiguration.java
@@ -1,13 +1,18 @@
 package com.nbug.depends.redis.config;
 
 import cn.hutool.core.util.ReflectUtil;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
@@ -35,11 +40,20 @@ public class XlwebRedisAutoConfiguration {
     }
 
     public static RedisSerializer<?> buildRedisSerializer() {
+        //设置序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         RedisSerializer<Object> json = RedisSerializer.json();
         // 解决 LocalDateTime 的序列化
-        ObjectMapper objectMapper = (ObjectMapper) ReflectUtil.getFieldValue(json, "mapper");
-        objectMapper.registerModules(new JavaTimeModule());
-        return json;
+        ObjectMapper om = (ObjectMapper) ReflectUtil.getFieldValue(json, "mapper");
+        om.registerModules(new JavaTimeModule());
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        // 不存在的字段,不被序列化
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        return jackson2JsonRedisSerializer;
     }
 
 }
