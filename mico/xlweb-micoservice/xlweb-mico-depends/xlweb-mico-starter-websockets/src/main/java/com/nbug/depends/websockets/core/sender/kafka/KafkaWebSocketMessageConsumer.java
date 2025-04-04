344033aa1af9c5a06fff4ecbc984466b3ac6264c
SeanChang <mmmmzhanglei@gmail.com>
2025-04-04 17:26:21 +0800

    xlweb-mico kafaka fix

--- mico/xlweb-micoservice/xlweb-mico-depends/xlweb-mico-starter-websockets/src/main/java/com/nbug/depends/websockets/core/sender/kafka/KafkaWebSocketMessageConsumer.java
+++ mico/xlweb-micoservice/xlweb-mico-depends/xlweb-mico-starter-websockets/src/main/java/com/nbug/depends/websockets/core/sender/kafka/KafkaWebSocketMessageConsumer.java
@@ -1,7 +1,6 @@
 package com.nbug.depends.websockets.core.sender.kafka;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 
 /**
@@ -14,7 +13,6 @@ public class KafkaWebSocketMessageConsumer {
 
     private final KafkaWebSocketMessageSender kafkaWebSocketMessageSender;
 
-    @RabbitHandler
     @KafkaListener(
             topics = "${xlweb.websocket.sender-kafka.topic}",
             // 在 Group 上，使用 UUID 生成其后缀。这样，启动的 Consumer 的 Group 不同，以达到广播消费的目的
